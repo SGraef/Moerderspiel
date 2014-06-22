eb46bedce8a1fea9cc6d2e4c8e0ea72b70cba255
Sascha Graef <sgraef@fb3.uni-bremen.de>
2014-06-22 15:11:51 +0200

    fill seeds with data
    for development process improvement adding dummy
    games and user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+gamemaster =  User.create(username: "gamemaster", password: "123456", email: "gamemaster@example.com")
+user1 =  User.create(username: "jim", password: "123456", email: "jim@example.com")
+user2 = User.create(username: "john", password: "123456", email: "john@bar.de")
+game = Game.create(name: "test1", circle_count: 2, gamemaster: gamemaster, starttime: Time.now, endtime: (Time.now+2.month),status: "open")
+game.participate(user1)
+game.participate(user2)
+game.participate(nil, "sam", "sam@example.com")
+
+game = Game.create(name: "test2", circle_count: 2, gamemaster: gamemaster, starttime: Time.now, endtime: (Time.now+2.month),status: "open")
+game.participate(user1)
+game.participate(user2)
+game.participate(nil, "sam", "sam@example.com")
+game.start
+
+game = Game.create(name: "test3", circle_count: 2, gamemaster: gamemaster, starttime: Time.now, endtime: (Time.now+2.month),status: "open")
+game.participate(user1)
+game.participate(user2)
+game.participate(nil, "sam", "sam@example.com")
+game.start
+game.end_game
